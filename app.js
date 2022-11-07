506df04250a177509eabbf0cb5ed6fc585427588
Cassandra Juif <cassandra.juif@gmail.com>
2022-11-07 12:21:47 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5001;
 
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
+const usersHandlers = require("./usersHandlers");
 
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
