fe3276c8bf3dcd4998440b3e7a716d7431b14604
Cassandra Juif <cassandra.juif@gmail.com>
2022-11-08 18:35:54 +0100

    Adding the post.

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const port = process.env.APP_PORT ?? 5001;
 
 const usersHandlers = require("./usersHandlers");
 
+app.use(express.json());
+
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/users", usersHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
